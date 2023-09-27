e2a6c62809db83b6e3057c5c434e6dc997c99c5b
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-27 09:43:26 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(games) {
+    const winGame = games.find(game => game.result === "W");
+    if (winGame) {
+      return winGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  superbowlWin();
+  
+
+
